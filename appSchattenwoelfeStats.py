a2ecd29c67424b1dfd53ab448f210fd1335181a9
Jacsi <96053932+Jacsi@users.noreply.github.com>
2022-03-18 14:14:54 +0100

    Update appSchattenwoelfeStats.py

--- appSchattenwoelfeStats.py
+++ appSchattenwoelfeStats.py
@@ -127,10 +127,11 @@ st.text("Im ausgewähltem Zeitraum wurden: " + str(xpbardiagramsum) + " XP gesam
 ###################################### END XP Calculation#################
 t = (dffilter[dffilter[7] == max(dffilter[7])][2])
 
-if (t.item() and (filtername != "DieSchattenwölfe")) :
-       st.text("Derzeit hat " + filtername + " die Questteilnahme aktiviert!")
-else:
-      st.text("Derzeit nimmt " + filtername + " NICHT an Quests teil! \nBitte aktiviere den Questharken, wenn du teilnehmen möchtest.")
+if (filtername != "DieSchattenwölfe"):
+    if (t.item() ) :
+           st.text("Derzeit hat " + filtername + " die Questteilnahme aktiviert!")
+    else:
+          st.text("Derzeit nimmt " + filtername + " NICHT an Quests teil! \nBitte aktiviere den Questharken, wenn du teilnehmen möchtest.")
 
 
 st.text("Die letzte Aktualisierung der Daten erfolgte am: " + str(df[7].max()) + "\nIn der Regel werden die Daten um 18:30 Uhr aktualisiert.")
